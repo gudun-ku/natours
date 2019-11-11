4b0dc173c1eddfb2fbcb42834ec2cb5783032e6d
Aleksandr Beloushkin <aleks.beloushkin@yahoo.com>
2019-11-11 18:06:54 +0300

    Adds Atlas DB connection, Tour model and schema

--- server.js
+++ server.js
@@ -1,3 +1,4 @@
+const mongoose = require('mongoose');
 const dotenv = require('dotenv');
 
 // ENVIRONMENT VARIABLES
@@ -7,6 +8,45 @@ const app = require('./app');
 //console.log(app.get('env'));
 //console.log(process.env);
 
+const DB = process.env.DATABASE.replace(
+  '<USERNAME>',
+  process.env.DATABASE_USER
+).replace('<PASSWORD>', process.env.DATABASE_PASSWORD);
+
+mongoose
+  .connect(DB, {
+    useNewUrlParser: true,
+    useCreateIndex: true,
+    useFindAndModify: false
+  })
+  .then(() => console.log('DB connection succesful!'));
+
+// TOUR SCHEMA (MODEL)
+const tourSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: [true, 'a tour must have a name'],
+    unique: true
+  },
+  price: { type: Number, required: [true, 'a tour must have a price'] },
+  rating: { type: Number, default: 4.5 }
+});
+
+const Tour = mongoose.model('Tour', tourSchema);
+
+const testTour = new Tour({
+  name: 'The Park Camper',
+  price: 297
+});
+
+testTour
+  .save()
+  .then(doc => {
+    console.log(doc);
+  })
+  .catch(err => {
+    console.log('ERROR: ', err);
+  });
 // START SERVER
 const port = process.env.PORT || 3000;
 app.listen(port, () => {
