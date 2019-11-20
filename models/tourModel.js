69c583f66884afc65694a4e2652b164f667e474c
Aleksandr Beloushkin <aleks.beloushkin@yahoo.com>
2019-11-20 18:10:09 +0300

    Adds query middleware

--- models/tourModel.js
+++ models/tourModel.js
@@ -1,4 +1,6 @@
 const mongoose = require('mongoose');
+const slugify = require('slugify');
+
 // TOUR SCHEMA (MODEL)
 const tourSchema = new mongoose.Schema(
   {
@@ -7,6 +9,7 @@ const tourSchema = new mongoose.Schema(
       required: [true, 'a tour must have a name'],
       unique: true
     },
+    slug: String,
     duration: {
       type: Number,
       required: [true, 'A tour must have a duration']
@@ -50,7 +53,11 @@ const tourSchema = new mongoose.Schema(
       default: Date.now(),
       select: false
     },
-    startDates: [Date]
+    startDates: [Date],
+    secretTour: {
+      type: Boolean,
+      default: false
+    }
   },
   {
     toJSON: { virtuals: true },
@@ -60,6 +67,39 @@ const tourSchema = new mongoose.Schema(
 
 tourSchema.virtual('durationWeeks').get(function() {
   return this.duration / 7;
+});
+
+// DOCUMENT MIDDLEWARE: runs before .save() and .create
+tourSchema.pre('save', function(next) {
+  this.slug = slugify(this.name, { lower: true });
+  next();
+});
+
+// we can use multiply middleware hooks
+// tourSchema.pre('save', function(next) {
+//   console.log('Document will be saved!');
+//   next();
+// });
+
+// Executed after all pre functions are completed
+
+// tourSchema.post('save', function(doc, next) {
+//   console.log(doc);
+//   next();
+// });
+
+// QUERY MIDDLEWARE
+// for find all and one we use regular expression
+tourSchema.pre(/^find/, function(next) {
+  this.find({ secretTour: { $ne: true } });
+  this.start = Date.now();
+  next();
+});
+
+tourSchema.post(/^find/, function(docs, next) {
+  console.log(`Query took ${Date.now() - this.start} milliseconds`);
+  console.log(docs);
+  next();
 });
 
 const Tour = mongoose.model('Tour', tourSchema);
