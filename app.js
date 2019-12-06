04035bf0050d7763f33cdf03e8e438aa0a5ed730
Aleksandr Beloushkin <aleks.beloushkin@yahoo.com>
2019-12-06 22:25:34 +0300

    Adds global error handler

--- app.js
+++ app.js
@@ -1,6 +1,8 @@
 const express = require('express');
 const morgan = require('morgan');
 
+const AppError = require('./utils/appError');
+const globalErrorHandler = require('./controllers/errorController');
 const tourRouter = require('./routes/tourRoutes');
 const userRouter = require('./routes/userRoutes');
 
@@ -30,10 +32,10 @@ app.use('/api/v1/users', userRouter);
 
 // NO ONE ROUTER HAS BEEN CATCHED - THAT NEED TO BE AFTER EACH ROUTE OF OUR APP
 app.all('*', (req, res, next) => {
-  res.status(404).json({
-    status: 'fail',
-    message: `Can't find ${req.originalUrl} on this server!`
-  });
+  next(new AppError(`Can't find ${req.originalUrl} on this server!`, 404));
 });
+
+// Errors handling
+app.use(globalErrorHandler);
 
 module.exports = app;
