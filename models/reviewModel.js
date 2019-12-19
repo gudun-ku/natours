f61091f54b90dfb5e28f3dbda2db43daa3d05e5d
Aleksandr Beloushkin <aleks.beloushkin@yahoo.com>
2019-12-19 12:35:51 +0300

    Adds rating averages calculations on review changes

--- models/reviewModel.js
+++ models/reviewModel.js
@@ -1,4 +1,5 @@
 const mongoose = require('mongoose');
+const Tour = require('./tourModel');
 
 // REVIEW SCHEMA (MODEL)
 const reviewSchema = new mongoose.Schema(
@@ -36,18 +37,64 @@ const reviewSchema = new mongoose.Schema(
   }
 );
 
+reviewSchema.index({ tour: 1, user: 1 }, { unique: true });
+
 reviewSchema.pre(/^find/, function(next) {
   this.populate({ path: 'user', select: 'name photo' });
-
   next();
+});
+
+// Static method for calculating rating stats for tour
+reviewSchema.statics.calcAverageRatings = async function(tourId) {
+  const stats = await this.aggregate([
+    {
+      $match: { tour: tourId }
+    },
+    {
+      $group: {
+        _id: '$tour',
+        nRating: { $sum: 1 },
+        avgRating: { $avg: '$rating' }
+      }
+    }
+  ]);
+  //console.log(stats);
+  if (stats.length > 0) {
+    await Tour.findByIdAndUpdate(tourId, {
+      ratingsQuantity: stats[0].nRating,
+      ratingsAverage: stats[0].avgRating
+    });
+  } else {
+    await Tour.findByIdAndUpdate(tourId, {
+      ratingsQuantity: 0,
+      ratingsAverage: 4.5
+    });
+  }
+};
+
+// POST - renew stats on new review creation
+reviewSchema.post('save', function() {
+  // this points to current doc - review
+  // we need to initialize current doc so use constructor
+  this.constructor.calcAverageRatings(this.tour);
+});
+
+// FindByIdAndUpdate
+// FindByIdAndDelete
+reviewSchema.pre(/^findOneAnd/, async function(next) {
+  // r to post function
+  this.r = await this.findOne();
+  next();
+});
+
+// We're passing data from pre middleware to post middleware with r var
+reviewSchema.post(/^findOneAnd/, async function() {
+  // await this.findOne() does NOT work here, query has already executed
+  if (this.r) {
+    await this.r.constructor.calcAverageRatings(this.r.tour);
+  }
 });
 
 const Review = mongoose.model('Review', reviewSchema);
 
 module.exports = Review;
-
-// WHAT WE WANT
-
-// POST /tour/34324a/reviews
-// GET  /tour/34324a/reviews
-// GET  /tour/34324a/reviews/0498750a
