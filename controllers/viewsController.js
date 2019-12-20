0dc15ed1c021091c225b45c22d10cae2fa315a26
Aleksandr Beloushkin <gudun.ku@gmail.com>
2019-12-20 23:44:47 +0300

    Adds tours overview page

--- controllers/viewsController.js
+++ controllers/viewsController.js
@@ -1,13 +1,23 @@
-//const catchAsync = require('./../utils/catchAsync');
+const catchAsync = require('./../utils/catchAsync');
+const Tour = require('./../models/tourModel');
 
-exports.getOvierview = (req, res) => {
+
+exports.getOvierview = catchAsync(async (req, res) => {
+  // 1) Get Tour data from collection
+  const tours = await Tour.find();
+
+  // 2) Build a template
+  // 3) Render that template using data from 1
+
   res.status(200).render('overview', {
-    title: 'All Tours'
+    title: 'All Tours',
+    tours
   });
-};
+});
 
 exports.getTour = (req, res) => {
   res.status(200).render('tour', {
     title: 'The Forest Hiker Tour'
   });
 };
+
