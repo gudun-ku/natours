f77fa1a621b01aa9ea15f897ee0e2393ce472d53
Aleksandr Beloushkin <aleks.beloushkin@yahoo.com>
2019-12-30 15:53:50 +0300

    Adds tour images uploading (API)

--- controllers/userController.js
+++ controllers/userController.js
@@ -35,20 +35,20 @@ const upload = multer({
 
 exports.uploadUserPhoto = upload.single('photo');
 
-exports.resizeUserPhoto = (req, res, next) => {
+exports.resizeUserPhoto = catchAsync(async (req, res, next) => {
   if (!req.file) return next();
 
   const ext = req.file.mimetype.split('/')[1];
   req.file.filename = `user-${req.user.id}-${Date.now()}.${ext}`;
 
-  sharp(req.file.buffer)
+  await sharp(req.file.buffer)
     .resize(500, 500)
     .toFormat('jpeg')
     .jpeg({ quality: 90 })
     .toFile(`public/img/users/${req.file.filename}`);
 
   next();
-};
+});
 
 //using handler factory
 const factory = require('./handlerFactory');
