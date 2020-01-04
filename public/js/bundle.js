b368628aa51a836e9b9f428516a0f84c90666401
Aleksandr Beloushkin <aleks.beloushkin@yahoo.com>
2020-01-04 22:15:57 +0300

    added basic mapbox functions

--- public/js/bundle.js
+++ public/js/bundle.js
@@ -6598,21 +6598,19 @@ try {
 }
 
 },{}],"mapbox.js":[function(require,module,exports) {
-"use strict";
-
-Object.defineProperty(exports, "__esModule", {
-  value: true
-});
-exports.displayMap = void 0;
-
 //console.log('Hello from the client side :D');
 
 /* eslint-disable */
-var displayMap = function displayMap(locations) {
-  return false;
-};
+var locations = JSON.parse(document.getElementById('map').dataset.locations);
+console.log(locations);
+mapboxgl.accessToken = 'pk.eyJ1IjoiZ3VkdW4ta3UiLCJhIjoiY2s0enlrbzVsMDl1OTNscW44bXMyN2I1YiJ9.u7yQCVVDUTxLiZJDhsc25g';
+var map = new mapboxgl.Map({
+  container: 'map',
+  style: 'mapbox://styles/gudun-ku/ck4zyopdd4y311cqltt7cyov6' // center: [-118.998, 34.111],
+  // zoom: 6,
+  // interactive: false
 
-exports.displayMap = displayMap;
+});
 },{}],"../../node_modules/axios/lib/helpers/bind.js":[function(require,module,exports) {
 'use strict';
 
@@ -8947,7 +8945,7 @@ var parent = module.bundle.parent;
 if ((!parent || !parent.isParcelRequire) && typeof WebSocket !== 'undefined') {
   var hostname = "" || location.hostname;
   var protocol = location.protocol === 'https:' ? 'wss' : 'ws';
-  var ws = new WebSocket(protocol + '://' + hostname + ':' + "1428" + '/');
+  var ws = new WebSocket(protocol + '://' + hostname + ':' + "3097" + '/');
 
   ws.onmessage = function (event) {
     checkedAssets = {};
--- public/js/mapbox.js
+++ public/js/mapbox.js
@@ -1,6 +1,15 @@
 //console.log('Hello from the client side :D');
 /* eslint-disable */
 
-export const displayMap = locations => {
-  return false;
-};
+const locations = JSON.parse(document.getElementById('map').dataset.locations);
+console.log(locations);
+
+mapboxgl.accessToken =
+  'pk.eyJ1IjoiZ3VkdW4ta3UiLCJhIjoiY2s0enlrbzVsMDl1OTNscW44bXMyN2I1YiJ9.u7yQCVVDUTxLiZJDhsc25g';
+var map = new mapboxgl.Map({
+  container: 'map',
+  style: 'mapbox://styles/gudun-ku/ck4zyopdd4y311cqltt7cyov6'
+  // center: [-118.998, 34.111],
+  // zoom: 6,
+  // interactive: false
+});
