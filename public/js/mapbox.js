195afeb5acb25f4729993c195005f5aeafb757a8
Aleksandr Beloushkin <aleks.beloushkin@yahoo.com>
2020-01-06 12:38:47 +0300

    Adds stripe payment client part

--- public/js/mapbox.js
+++ public/js/mapbox.js
@@ -1,51 +1,55 @@
 //console.log('Hello from the client side :D');
 /* eslint-disable */
+const mapBox = document.getElementById('map');
+if (mapBox) {
+  const locations = JSON.parse(
+    document.getElementById('map').dataset.locations
+  );
+  //console.log(locations);
 
-const locations = JSON.parse(document.getElementById('map').dataset.locations);
-//console.log(locations);
+  mapboxgl.accessToken =
+    'pk.eyJ1IjoiZ3VkdW4ta3UiLCJhIjoiY2s0enlrbzVsMDl1OTNscW44bXMyN2I1YiJ9.u7yQCVVDUTxLiZJDhsc25g';
+  var map = new mapboxgl.Map({
+    container: 'map',
+    style: 'mapbox://styles/gudun-ku/ck4zyopdd4y311cqltt7cyov6',
+    scrollZoom: false
+    // center: [-118.998, 34.111],
+    // zoom: 6,
+    // interactive: false
+  });
 
-mapboxgl.accessToken =
-  'pk.eyJ1IjoiZ3VkdW4ta3UiLCJhIjoiY2s0enlrbzVsMDl1OTNscW44bXMyN2I1YiJ9.u7yQCVVDUTxLiZJDhsc25g';
-var map = new mapboxgl.Map({
-  container: 'map',
-  style: 'mapbox://styles/gudun-ku/ck4zyopdd4y311cqltt7cyov6',
-  scrollZoom: false
-  // center: [-118.998, 34.111],
-  // zoom: 6,
-  // interactive: false
-});
+  const bounds = new mapboxgl.LngLatBounds();
 
-const bounds = new mapboxgl.LngLatBounds();
+  locations.forEach(loc => {
+    // Create marker
+    const el = document.createElement('div');
+    el.className = 'marker';
 
-locations.forEach(loc => {
-  // Create marker
-  const el = document.createElement('div');
-  el.className = 'marker';
+    // Add marker
+    new mapboxgl.Marker({
+      element: el,
+      anchor: 'bottom'
+    })
+      .setLngLat(loc.coordinates)
+      .addTo(map);
 
-  // Add marker
-  new mapboxgl.Marker({
-    element: el,
-    anchor: 'bottom'
-  })
-    .setLngLat(loc.coordinates)
-    .addTo(map);
+    // Add popup
+    new mapboxgl.Popup({
+      offset: 30
+    })
+      .setLngLat(loc.coordinates)
+      .setHTML(`<p>Day ${loc.day}: ${loc.description}</p>`)
+      .addTo(map);
 
-  // Add popup
-  new mapboxgl.Popup({
-    offset: 30
-  })
-    .setLngLat(loc.coordinates)
-    .setHTML(`<p>Day ${loc.day}: ${loc.description}</p>`)
-    .addTo(map);
+    bounds.extend(loc.coordinates);
+  });
 
-  bounds.extend(loc.coordinates);
-});
-
-map.fitBounds(bounds, {
-  padding: {
-    top: 200,
-    bottom: 150,
-    left: 100,
-    right: 100
-  }
-});
+  map.fitBounds(bounds, {
+    padding: {
+      top: 200,
+      bottom: 150,
+      left: 100,
+      right: 100
+    }
+  });
+}
---
+++ public/js/stripe.js
@@ -0,0 +1,23 @@
+import axios from 'axios';
+import { showAlert } from './alert';
+
+/* eslint-disable */
+const stripe = Stripe('pk_test_CoiUwZu0VPLoZ8WiijlR0OJk00dCZn65pZ');
+
+export const bookTour = async tourId => {
+  try {
+    // 1) Get the checkout session from the server API
+    const session = await axios(
+      `http://localhost:8000/api/v1/bookings/checkout-session/${tourId}`
+    );
+    console.log(session);
+
+    // 2) Create checkout form  + charge credit card
+    await stripe.redirectToCheckout({
+      sessionId: session.data.session.id
+    });
+  } catch (err) {
+    console.log(err);
+    showAlert('error', err);
+  }
+};
