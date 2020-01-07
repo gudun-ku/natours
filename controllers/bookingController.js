7bb955cf462af1108c65fa7141a5107633a6f43d
Aleksandr Beloushkin <aleks.beloushkin@yahoo.com>
2020-01-07 15:06:18 +0300

    Adds tour booking test hook

--- controllers/bookingController.js
+++ controllers/bookingController.js
@@ -1,5 +1,7 @@
 const stripe = require('stripe')(process.env.STRIPE_SECRET_KEY);
 const Tour = require('./../models/tourModel');
+const Booking = require('./../models/bookingModel');
+
 const catchAsync = require('./../utils/catchAsync');
 const AppError = require('./../utils/appError');
 
@@ -12,7 +14,10 @@ exports.getCheckoutSession = catchAsync(async (req, res, next) => {
   // 2) Create checkout session
   const session = await stripe.checkout.sessions.create({
     payment_method_types: ['card'],
-    success_url: `${req.protocol}://${req.get('host')}/`,
+    // workaround for testing, needs to remove in production
+    success_url: `${req.protocol}://${req.get('host')}/?tour=${
+      req.params.tourID
+    }&user=${req.user.id}&price=${tour.price}`,
     cancel_url: `${req.protocol}://${req.get('host')}/tour/${tour.slug}`,
     customer_email: req.user.email,
     client_reference_id: req.params.tourID,
@@ -32,4 +37,14 @@ exports.getCheckoutSession = catchAsync(async (req, res, next) => {
     status: 'success',
     session
   });
+});
+
+exports.createBookingCheckout = catchAsync(async (req, res, next) => {
+  const { tour, user, price } = req.query;
+  if (!tour || !user || !price) return next();
+
+  await Booking.create({ tour, user, price });
+
+  //redirect to root url - as part of workaround
+  res.redirect(req.originalUrl.split('?')[0]);
 });
--- routes/viewRoutes.js
+++ routes/viewRoutes.js
@@ -1,12 +1,18 @@
 const express = require('express');
 const viewsController = require('./../controllers/viewsController');
 const authController = require('./../controllers/authController');
+const bookingController = require('./../controllers/bookingController');
 
 const router = express.Router();
 
 //router.use(authController.isLoggedIn);
-
-router.get('/', authController.isLoggedIn, viewsController.getOvierview);
+router.get(
+  '/',
+  // workaround for testing with bookingController
+  bookingController.createBookingCheckout,
+  authController.isLoggedIn,
+  viewsController.getOvierview
+);
 
 router.get('/tour/:slug', authController.isLoggedIn, viewsController.getTour);
 
