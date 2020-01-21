4b4152e763be709f2ccfaa10c5c4f48293266dfc
Aleksandr Beloushkin <aleks.beloushkin@yahoo.com>
2020-01-21 18:23:30 +0300

    Prepares for publish - second stage

--- public/js/stripe.js
+++ public/js/stripe.js
@@ -7,9 +7,7 @@ const stripe = Stripe('pk_test_CoiUwZu0VPLoZ8WiijlR0OJk00dCZn65pZ');
 export const bookTour = async tourId => {
   try {
     // 1) Get the checkout session from the server API
-    const session = await axios(
-      `http://localhost:8000/api/v1/bookings/checkout-session/${tourId}`
-    );
+    const session = await axios(`/api/v1/bookings/checkout-session/${tourId}`);
     //console.log(session);
 
     // 2) Create checkout form  + charge credit card
